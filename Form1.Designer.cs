f0e90ae5b83770d186c693f7a46f140472d8af0a
Арсений Сендецкий <ars_121229@mail.ru>
2021-10-11 13:53:37 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace guif
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SumForRub = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Count = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Task = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SumForRub
+            // 
+            this.SumForRub.Location = new System.Drawing.Point(12, 33);
+            this.SumForRub.Name = "SumForRub";
+            this.SumForRub.Size = new System.Drawing.Size(195, 27);
+            this.SumForRub.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите стоимость в копейках";
+            // 
+            // Count
+            // 
+            this.Count.Location = new System.Drawing.Point(109, 66);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(98, 29);
+            this.Count.TabIndex = 2;
+            this.Count.Text = "Рассчитать";
+            this.Count.UseVisualStyleBackColor = true;
+            this.Count.Click += new System.EventHandler(this.Count_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(12, 66);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(91, 29);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Task
+            // 
+            this.Task.Location = new System.Drawing.Point(288, 145);
+            this.Task.Name = "Task";
+            this.Task.Size = new System.Drawing.Size(77, 29);
+            this.Task.TabIndex = 4;
+            this.Task.Text = "Задание";
+            this.Task.UseVisualStyleBackColor = true;
+            this.Task.Click += new System.EventHandler(this.Task_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 186);
+            this.Controls.Add(this.Task);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SumForRub);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Рассчет стоимости в рублях";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SumForRub;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Count;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Task;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guif
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void func()
+        {
+            int p = int.Parse(this.SumForRub.Text);
+            string result = "";
+            if (p < 1)
+                MessageBox.Show("Цена не может быть меньше 1 копейки!");
+            else if (p > 9999)
+                MessageBox.Show("Цена слишком высокая, не удовлетворяет условию задачи!");
+            else
+            {
+                if (p % 10 == 0) result = "Товар стоит " + p / 100 + " руб";
+                if (p / 100 == 1 || (p / 100) % 10 == 1 && p / 1000 != 1) result = "Товар стоит " + p / 100 + " руб" + "ль ";
+                else if (p / 100 == 2 || p / 100 == 3 || p / 100 == 4 || (p / 100) % 10 == 2 || (p / 100) % 10 == 3 || (p / 100) % 10 == 4)
+                {
+                    if (p / 100 == 1 || p / 1000 == 1) result = "Товар стоит " + p + " руб" + "лей ";
+                    else result = "Товар стоит " + p / 100 + " руб" + "ля ";
+                }
+                else if (p % 1000 == 200 || p % 1000 == 300 || p % 1000 == 400)
+                    result = "Товар стоит " + p / 100 + " руб" + "ля ";
+                else result = "Товар стоит " + p / 100 + " руб" + "лей ";
+                if (p % 100 == 0) MessageBox.Show(result);
+                else
+                {
+                    result = result + p % 100 + " копе";
+                    if (p % 10 == 1 && p % 100 != 11)
+                        result = result + "йка";
+                    else if ((p % 10 == 2 || p % 10 == 3 || p % 10 == 4) && p % 100 / 10 != 1)
+                        result = result + "йки";
+                    else result = result + "ек";
+                    MessageBox.Show(result);
+                }
+            }
+        }
+        private void Count_Click(object sender, EventArgs e)
+        {
+            func();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            this.SumForRub.Text = "";
+        }
+
+        private void Task_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Дано натуральное число 1≤n≤9999, определяющее стоимость товара в копейках. Выразить стоимость в рублях и копейках, например, 3 рубля 21 копейка, 15 рублей 5 копеек, 1 рубль ровно и т.п.");
+        }
+    }
+}
